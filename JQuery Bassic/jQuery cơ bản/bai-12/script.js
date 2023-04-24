b7994b1e6424d223cd83cbe697ac723ed601e6bc
Justin-Nova <rongluakhocnhe26@gmail.com>
2023-04-25 02:29:55 +0700

    cmt

---
+++ JQuery Bassic/jQuery cơ bản/bai-12/script.js
@@ -0,0 +1,44 @@
+//  vidu 1
+$("#button1").click(function () {
+
+    $("#example1").css({"width": "10px",
+        "height": "10px",
+        "background":"orangered"});
+
+    $("#example1").animate(
+        {"width": "100%", "height": "100px"}, 2000);
+});
+
+$("div").animate({
+    width: '+=250px',
+    height: '+=250px'
+  }, 1000);
+
+//   vidu 2
+
+function animate_examp() {
+
+    var mr = $("#example2").css("margin-left");
+    if (mr == '0px')
+        $("#example2").animate({"margin-left": "320px"}, 2000, "linear", function () {
+            animate_examp();
+        });
+    else
+        $("#example2").animate({"margin-left": "0px"}, 2000, "linear", function () {
+            animate_examp();
+        });
+
+  }
+
+
+$("#button2").click(function () {
+    animate_examp();
+    $(this).hide();
+});
+
+$("#button3").click(function () {
+
+    $("#example2").stop();
+    $("#button2").show();
+
+});
